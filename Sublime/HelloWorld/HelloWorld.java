060886bac6709b77d70cf82982dff6e76a91c26b
hugonasciutti <hugonasciutti@hotmail.com>
2017-02-25 01:36:03 -0300

    Java no Sublime

---
+++ Sublime/HelloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+	public HelloWorld() {
+		System.out.println("Hello World by Constructor HelloWorld()!");
+		
+	}
+	public static void main(String args[]){
+		HelloWorld hello = new HelloWorld();
+		System.out.println("Hello World by main!");
+	}
+
+}
