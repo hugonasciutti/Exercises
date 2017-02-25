ce4ffa31502f6cb5c91d3025b97d0345b04b5f4b
hugonasciutti <hugonasciutti@hotmail.com>
2017-02-25 01:40:37 -0300

    Little fix

--- Sublime/HelloWorld/HelloWorld.java
+++ Sublime/HelloWorld/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 		System.out.println("Hello World by Constructor HelloWorld()!");
 		
 	}
-	public static void main(String args[]){
+	public static void main(String[] args){
 		HelloWorld hello = new HelloWorld();
 		System.out.println("Hello World by main!");
 	}
