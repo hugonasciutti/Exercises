2024dd7616bb09d650bf07b484ec8ad6c77b1b08
Hugo Nasciutti <hugonasciutti@gmail.com>
2017-03-11 17:59:07 -0300

    Node.js progress

---
+++ Node.js/scope/config.js
@@ -0,0 +1,5 @@
+// config.js 
+// it is usually used to set global variables for your projects;
+
+// Remember that when not declared with var ou let it is global; 
+exports.maxValue = 10000;
--- Node.js/scope/index.js
+++ Node.js/scope/index.js
@@ -1,5 +1,8 @@
 var serialGenerator = require('./module.js');
-console.log(`nomeDaVariavelGlobal = ${nomeDaVariavelGlobal}`);
+// Use config.js to set your global variables, keep organized.
+var config = require('./config');
+console.log(`Global: config.maxValue = ${config.maxValue}`);
+console.log(`Global: nomeDaVariavelGlobal = ${nomeDaVariavelGlobal}`);
 // console.log(nomeDaVariavelSemVarOuLet);
 // console.log(privado1);
 
