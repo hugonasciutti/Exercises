9eb4c35a2a6fcc1e499215dbaa73d9343ee82d03
hugonasciutti <hugonasciutti@gmail.com>
2017-03-14 18:20:05 -0300

    React

---
+++ Node.js/process/isTTY.js
@@ -0,0 +1,10 @@
+let konsole = {
+  log: function(msg) {
+    process.stdout.write(`${msg}\n`);
+  },
+  error: function(msg) {
+    process.stderr.write(`${msg}\n`);
+  }
+};
+konsole.log('A TTY: ' + !!process.stdout.isTTY);
+konsole.error('B TTY: ' + !!process.stderr.isTTY);
