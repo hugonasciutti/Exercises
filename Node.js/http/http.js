acba416df101288fd0255665331cb05c8f4d9e07
Hugo Nasciutti <hugonasciutti@gmail.com>
2017-04-05 12:51:24 -0300

    Nodejs http

---
+++ Node.js/http/http.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+function onRequest(request, response) {
+  console.log('A user made a request' + request.url);
+  response.writeHead(200, {"Context-Type": "text/plain" });
+  response.write("Here is some data");
+  response.end();
+}
+
+http.createServer(onRequest).listen(8888);
+console.log('Server is now running...');
