297c73b4ea353e4f0423e4f17dfa078551d4ea1f
Hugo Nasciutti <hugonasciutti@gmail.com>
2017-04-07 17:58:56 -0300

    Nodemon and ES6]

--- Node.js/express_framework/1/README.md
+++ Node.js/express_framework/1/README.md
@@ -1,2 +1,8 @@
 # 1
 Testing the express framework with pug/jade
+
+You will need to have:
+
+`$ npm install -g npm-run-all`
+
+npm-run-all will allow you to run multiple scrips from your project.
---
+++ Node.js/express_framework/1/public/javascripts/carousel.js
@@ -0,0 +1,8 @@
+function readMouseMove() {
+  var result_x = document.getElementById('x_result');
+  var result_y = document.getElementById('y_result');
+  result_x.innerHTML = e.clientX;
+  result_y.innerHTML = e.clientY;
+}
+
+document.onmousemove = readMouseMove;
