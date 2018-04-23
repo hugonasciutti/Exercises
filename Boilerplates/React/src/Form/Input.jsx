7deeb680f69cfbbf70b9726f797a2f71fe625948
Hugo Nasciutti <hugonasciutti@gmail.com>
2018-04-23 15:17:23 -0300

    fix

---
+++ Boilerplates/React/src/Form/Input.jsx
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react'
+
+class Input extends PureComponent {
+  render() {
+    return <input {...this.props} />
+  }
+}
+
+export default Input
--- Boilerplates/React/src/index.jsx
+++ Boilerplates/React/src/index.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+import Form from './Form'
 
 class App extends Component {
   render() {
     return (
       <div>
-        Boilerplate
+        <Input />
       </div>
     )
   }
