4434406773a6c64a4edd25aece5eb75c90fdfd43
Hugo Nasciutti <hugonasciutti@gmail.com>
2018-04-27 14:46:31 -0300

    min

--- GraphQL/01/Client/webpack.config.js
+++ GraphQL/01/Client/webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   module: {
     rules: [{ test: /\.(js|jsx)?$/, loader: 'babel-loader', exclude: /node_modules/ }]
